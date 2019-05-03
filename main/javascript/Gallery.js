81f5b895335366697ba70e6aa9671f82c48a460d
kstulgys <karolis.stulgys@gmail.com>
2019-05-03 17:54:45 +1000

    wip

--- main/javascript/Gallery.js
+++ main/javascript/Gallery.js
@@ -5,21 +5,49 @@
       this._view = new ViewClass(this.onViewReady.bind(this))
       this.aborter = null
       this.moduleId = 'flickr'
-      // this.doSearch(this.state.query, this.state.moduleId)
+      this.images = []
+      this.likes = []
+      this._getLikesFromStorage()
+    }
+
+    _getLikesFromStorage() {
+      const likes = JSON.parse(localStorage.getItem('likes'))
+      this.likes = likes || []
+    }
+
+    _clearStorage() {
+      this.likes = []
+      this._onSearchResultReady({ images: this.images })
+      localStorage.clear()
+      this._view.clearLikes()
     }
 
     onViewReady() {
-      console.log('view ready!')
-      var { form, input, dropDown } = this._view.cache
+      console.log('Gallery view is ready!')
+      this._view.setLikes(this.likes)
+
+      var {
+        form,
+        input,
+        dropDown,
+        overlay,
+        likes,
+        clearLikes
+      } = this._view.cache
+      input.focus()
 
       form.onsubmit = e => {
         e.preventDefault()
         this.doSearch(input.value, this.moduleId)
         input.value = ''
       }
-      dropDown.onchange = e => {
-        this._onModuleChange(e)
-      }
+
+      dropDown.onchange = e => this._onModuleChange(e)
+      overlay.onclick = e => this._view.closeImage()
+      likes.onclick = e => this._view.viewImage(e.target.src)
+      clearLikes.onclick = () => this._clearStorage()
+
+      this.doSearch('lithuania', this.moduleId)
     }
 
     doSearch(query, moduleId) {
@@ -35,22 +63,59 @@
     }
 
     _onSearchResultReady({ images }) {
-      if (images.length === 0) {
+      this.images = images.map(img => {
+        if (this._isLiked(img.id)) {
+          return { ...img, liked: true }
+        } else {
+          return { ...img, liked: false }
+        }
+      })
+      if (this.images.length === 0) {
         this._view.showNoResults()
       } else {
-        this._view.addSearchResultsToView(images)
-        var { allImages } = this._view.cache
-        allImages.forEach(image => {
-          image.onclick = e => {
-            alert('clicked')
-          }
-        })
+        this._view.addSearchResultsToView(this.images)
+        var { results } = this._view.cache
+        results.onclick = e => {
+          this._handleResultsOnClick(e)
+        }
+      }
+    }
+
+    _handleResultsOnClick(e) {
+      const { id } = e.target.closest('.image-container').dataset
+
+      if (e.target.matches('.like-image, .like-image *')) {
+        const likeSpan = e.target.closest('.like-image')
+        this._addToLikes(this._findImage(id), likeSpan)
+      }
+      if (e.target.matches('.view-image, .view-image *')) {
+        this._view.viewImage(this._findImage(id).url)
+      }
+    }
+
+    _findImage(id) {
+      return this.images.find(img => img.id === id)
+    }
+
+    _addToLikes(image, likeSpan) {
+      if (!this._isLiked(image.id)) {
+        this.likes = [...this.likes, image]
+        localStorage.setItem('likes', JSON.stringify(this.likes))
+        this._view.addToLikes(image.url, likeSpan)
+      } else {
+        this.likes = this.likes.filter(_image => _image.id !== image.id)
+        localStorage.setItem('likes', JSON.stringify(this.likes))
+        this._view.removeFromLikes(this.likes, likeSpan)
       }
     }
 
     _onModuleChange(e) {
       this.moduleId = e.target.value
       this._view.focusInput()
+    }
+
+    _isLiked(id) {
+      return this.likes.findIndex(image => image.id === id) > -1
     }
   }
 
--- main/javascript/View.js
+++ main/javascript/View.js
@@ -12,12 +12,14 @@
       this.cache.input = document.querySelector('.search__field')
       this.cache.dropDown = document.querySelector('select')
       this.cache.overlay = document.querySelector('.overlay')
+      this.cache.likes = document.querySelector('.likes__list')
+      this.cache.clearLikes = document.querySelector('.clear-likes-btn')
+
       this.onViewReady()
     }
 
     addSearchResultsToView(images) {
       this.cache.results.innerHTML = ''
-
       images.forEach(image => {
         this.cache.results.insertAdjacentHTML(
           'beforeend',
@@ -25,13 +27,25 @@
         )
       })
 
-      this.cache.allImages = [...document.querySelectorAll('.image')]
+      this.cache.allImages = [...document.querySelectorAll('.image-container')]
     }
 
-    renderImage(image) {
+    renderImage({ url, id, liked }) {
       return `
-        <div class="image-container">
-          <img src=${image.url} class="image" />
+        <div class="image-container" data-id=${id}>
+          <img src=${url} class="image"/>
+          <div class='image-action'>
+            <span class='like-image'>
+            ${
+              liked
+                ? `<i class="fas fa-heart fa-6x"></i>`
+                : `<i class="far fa-heart fa-6x"></i>`
+            }
+            </span>
+            <span class='view-image'>
+              <i class="far fa-eye fa-6x"></i>
+            </span>
+          </div>
         </div>
       `
     }
@@ -41,14 +55,55 @@
     }
 
     showNoResults() {
+      const markup = `<div class='no-results'>
+        <h1>No results found :(</h1>
+      </div>`
       this.cache.results.innerHTML = ''
-      this.cache.results.insertAdjacentHTML(
-        'afterbegin',
-        `<div class='no-results'>
-          <h1>No results found :(</h1>
-         </div>`
-      )
-      // document.querySelector('.no-results').classList.add('active')
+      this.cache.results.insertAdjacentHTML('afterbegin', markup)
+    }
+
+    setLikes(images) {
+      images.forEach(image => {
+        this.addToLikes(image.url)
+      })
+    }
+
+    viewImage(image) {
+      this.cache.overlay.innerHTML = ''
+      this.cache.overlay.style.visibility = 'visible'
+      this.cache.overlay.insertAdjacentHTML('afterbegin', `<img src=${image}/>`)
+    }
+
+    closeImage() {
+      this.cache.overlay.style.visibility = 'hidden'
+      this.cache.overlay.innerHTML = ''
+    }
+
+    addToLikes(url, likeSpan) {
+      const markup = `
+        <li>
+          <img class='liked-image' src=${url}/>
+        </li>
+      `
+      this.cache.likes.insertAdjacentHTML('beforeend', markup)
+      if (likeSpan) {
+        likeSpan.innerHTML = `<i class="fas fa-heart fa-6x"></i>`
+      }
+    }
+
+    removeFromLikes(likes, likeSpan) {
+      this.cache.likes.innerHTML = ''
+      likes.forEach(({ url }) => {
+        this.addToLikes(url)
+      })
+
+      if (likeSpan) {
+        likeSpan.innerHTML = `<i class="far fa-heart fa-6x"></i>`
+      }
+    }
+
+    clearLikes() {
+      this.cache.likes.innerHTML = ''
     }
 
     render() {
@@ -90,7 +145,8 @@
                       <ul class="likes__list">
                         <!-- liked images -->
                       </ul>
-                    </div>
+                      <button class='clear-likes-btn'>Clear likes</button>
+                      </div>
                   </div>
                 </header>
                 <!-- end of header -->
@@ -111,186 +167,6 @@
               `
     }
   }
-
-  // var View = function() {}
-  // View.prototype = {
-  //   startLoaderAndDoCleanup() {
-  //     this.startLoader()
-  //     this._resultsNode.innerHTML = ''
-  //     this._noResultsNode.style.display = 'none'
-  //   },
-
-  //   stopLoaderAndDoCleanup() {
-  //     this.stopLoader()
-  //     this._noResultsNode.style.display = 'none'
-  //     this._noResultsNode.style.opacity = '0'
-  //   },
-
-  //   stopLoader() {
-  //     this._spinnerNode.style.display = 'none'
-  //     this._queryInputNode.value = ''
-  //   },
-
-  //   startLoader() {
-  //     this._spinnerNode.style.display = 'initial'
-  //   },
-
-  //   getInputText(e) {
-  //     return e.target.value
-  //   },
-
-  //   showNoResults() {
-  //     this.stopLoaderAndDoCleanup()
-  //     this._noResultsNode.style.display = 'initial'
-  //     this._noResultsNode.style.opacity = '1'
-  //     this._noResultsNode.style.transform = 'translateX(0)'
-  //   },
-
-  //   /**
-  //    * update gallery content with search results
-  //    * @param {[{id:String, url:string, title:string}]}} results -  array of images for gallery update
-  //    */
-  //   addSearchResultsToView(results) {
-  //     const resultsLength = results.length
-  //     let loaded = 0
-  //     results.forEach((item, i) => {
-  //       let imgNode = document.createElement('img')
-  //       imgNode.setAttribute('src', item.url)
-  //       imgNode.setAttribute('class', 'gallery-image')
-  //       imgNode.addEventListener('click', e => this.onImageClick(e))
-  //       imgNode.onload = () => {
-  //         loaded++
-  //         this._imageCounter.innerHTML = `${loaded}/${resultsLength}`
-  //         this._resultsNode.appendChild(imgNode)
-  //         if (resultsLength >= 12) {
-  //           if (loaded === 12) {
-  //             this.stopLoaderAndDoCleanup()
-  //           }
-  //         } else {
-  //           this.stopLoaderAndDoCleanup()
-  //         }
-  //       }
-  //     })
-  //   },
-
-  // focusInput() {
-  //   this._queryInputNode.focus()
-  // },
-
-  //   onImageClick(e) {
-  //     this._modalImgNode.setAttribute('src', e.target.src)
-  //     Object.assign(this._modalNode.style, {
-  //       visibility: 'visible',
-  //       opacity: '1'
-  //     })
-  //   },
-
-  //   /**
-  //    * adds gallery main view node as child node
-  //    * @param {htmlElement} node - html element to append to
-  //    */
-  //   addToNode(node) {
-  //     node.appendChild(this._viewNode)
-  //   },
-
-  //   onOutsideImageClick(e) {
-  //     Object.assign(this._modalNode.style, {
-  //       visibility: 'hidden',
-  //       opacity: '0'
-  //     })
-  //   },
-
-  //   /**
-  //    * add event event listeners to the DOM and triger default search
-  //    * @param {Object} galleryThis - gallery instance
-  //    */
-  //   setFunctionality(galleryThis) {
-  //     this._queryInputNode.addEventListener('change', e => {
-  //       e.preventDefault()
-  //       galleryThis._onSearchButtonClick(e)
-  //     })
-
-  //     this._formNode.addEventListener('submit', e => {
-  //       e.preventDefault()
-  //     })
-
-  //     this._dropDownNode.addEventListener('change', e => {
-  //       console.log(e.target.value)
-  //       galleryThis._onModuleChange(e)
-  //     })
-
-  //     this._modalNode.addEventListener('click', e => {
-  //       this.onOutsideImageClick(e)
-  //     })
-
-  //     this.startLoaderAndDoCleanup()
-  //     var { query, selectedModuleId } = galleryThis.state
-  //     galleryThis.doSearch(query, selectedModuleId)
-  //   },
-
-  //   /**
-  //    * creates gallery view, inner structure and ui
-  //    */
-  //   createInterface() {
-  //     this._viewNode = document.createElement('div')
-  //     this._viewNode.classList.add('gallery')
-
-  //     this._controlsNode = document.createElement('div')
-  //     this._controlsNode.classList.add('gallery-controls')
-  //     this._viewNode.appendChild(this._controlsNode)
-
-  //     this._noResultsNode = document.createElement('h1')
-  //     this._noResultsNode.classList.add('no-results')
-  //     this._noResultsNode.innerHTML = 'Sorry, no results found :-S'
-  //     this._viewNode.appendChild(this._noResultsNode)
-
-  //     this._formNode = document.createElement('form')
-  //     this._queryInputNode = document.createElement('input')
-  //     this._queryInputNode.placeholder = 'Search for anything...'
-  //     this._queryInputNode.autofocus = true
-
-  //     this._searchBtnNode = document.createElement('button')
-  //     this._searchBtnNode.innerHTML = '&rArr;'
-  //     this._formNode.appendChild(this._queryInputNode)
-  //     this._formNode.appendChild(this._searchBtnNode)
-  //     this._controlsNode.appendChild(this._formNode)
-
-  //     this._dropDownNode = document.createElement('select')
-  //     this._option1Node = document.createElement('option')
-  //     this._option2Node = document.createElement('option')
-  //     this._option1Node.value = 'static'
-  //     this._option1Node.innerHTML = 'static'
-
-  //     this._option2Node.value = 'flickr'
-  //     this._option2Node.innerHTML = 'flickr'
-  //     this._option2Node.selected = 'selected'
-
-  //     this._dropDownNode.appendChild(this._option1Node)
-  //     this._dropDownNode.appendChild(this._option2Node)
-  //     this._controlsNode.appendChild(this._dropDownNode)
-
-  //     this._imageCounter = document.createElement('p')
-  //     this._imageCounter.innerHTML = ''
-  //     this._controlsNode.appendChild(this._imageCounter)
-
-  //     this._resultsNode = document.createElement('div')
-  //     this._resultsNode.classList.add('gallery-items')
-  //     this._viewNode.appendChild(this._resultsNode)
-
-  //     this._modalNode = document.createElement('div')
-  //     this._modalNode.classList.add('modal')
-  //     this._viewNode.appendChild(this._modalNode)
-
-  //     this._modalImgNode = document.createElement('img')
-  //     this._modalImgNode.classList.add('modal-img')
-  //     this._modalNode.appendChild(this._modalImgNode)
-
-  //     this._spinnerNode = document.createElement('img')
-  //     this._spinnerNode.classList.add('spinner')
-  //     this._spinnerNode.src = '../resources/loader.svg'
-  //     this._viewNode.appendChild(this._spinnerNode)
-  //   }
-  // }
 
   window.CLASSES.ViewClass = View
 })(window)
